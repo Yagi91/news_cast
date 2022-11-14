828e56017a58b28c3310c9bc9ca6d3eba348e78f
Dodley <dodleydesign+@gmail.com>
2022-11-14 14:25:03 +0100

    Completely designed navigation bar and added extra assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./dist/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        ncDark: "#393939",
+        primary: {
+          400: "#F86D6D",
+          500: "#F65050",
+          600: "#F53D3D",
+        },
+      },
+      fontFamily: {
+        header: ["Oswald", "sans-serif"],
+      },
+    },
   },
+
   plugins: [],
 };
